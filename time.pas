c3fc187c516ee1b089f2a7eb227bcefc7761c504
Роман Морозов <ramrost666@gmail.com>
2024-10-05 10:34:05 +0300

    вывод дней в году в time.pas + timer: суммарное кол-во секунд

--- time.pas
+++ time.pas
@@ -1,5 +1,5 @@
 ﻿begin
   var y := ReadInteger('Введити год:');
-  if ((y mod 4 = 0) and (y mod 100 <> 0) and (y mod 400 <> 0)) then PrintLn('Это високосный год')
-  else PrintLn('Это не високосный год');
+  if ((y mod 4 = 0) and (y mod 100 <> 0) and (y mod 400 <> 0)) then PrintLn('366 дней')
+  else PrintLn('365 дней');
 end.
---
+++ timer.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var h := ReadInteger('');
+  var m := ReadInteger('');
+  var s := ReadInteger('');
+  var sm := h*3600 + m*60 + s;
+  PrintLn($'Кол-во секунд: {sm}'); 
+end.S
