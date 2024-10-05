bf7c07d3b4ea3c1878c79dc1cf55c368c2d06e4f
Роман Морозов <ramrost666@gmail.com>
2024-10-05 10:26:48 +0300

    проверка високосности

---
+++ time.pas
@@ -0,0 +1,5 @@
+﻿begin
+  var y := ReadInteger('Введити год:');
+  if ((y mod 4 = 0) and (y mod 100 <> 0) and (y mod 400 <> 0)) then PrintLn('Это високосный год')
+  else PrintLn('Это не високосный год');
+end.
